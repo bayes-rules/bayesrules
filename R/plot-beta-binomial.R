434abad58eecee8d9ff6beebc02b3bad64d5c94e
mdogucu <mdogucu@gmail.com>
2020-01-03 09:58:48 -0800

    some stuff

--- R/plot-beta-binomial.R
+++ R/plot-beta-binomial.R
@@ -1,16 +1,51 @@
-plot_beta_binomial <- function (alpha, beta, x = NULL, n = NULL, prior = TRUE, likelihood = TRUE,
-                                posterior = TRUE)
-{
+#' Plot Bayesian Beta Binomial Models
+#'
+#' @param alpha
+#' @param beta
+#' @param x
+#' @param n
+#' @param prior
+#' @param likelihood
+#' @param posterior
+#'
+#' @return
+#' @export
+#'
+#' @examples
+plot_beta_binomial <- function (alpha,
+                                beta,
+                                x = NULL,
+                                n = NULL,
+                                prior = TRUE,
+                                likelihood = TRUE,
+                                posterior = TRUE){
   if (is.null(x) | is.null(n))
-    warning("To visualize the posterior, specify data x and n")
-  g <- ggplot(NULL, aes(x = c(0, 1))) + labs(x = expression(pi),
-                                             y = "density") + scale_fill_manual("", values = c(prior = "gold1",
-                                                                                               `(scaled) likelihood` = "cyan2", posterior = "cyan4"), breaks = c("prior", "(scaled) likelihood", "posterior"))
+    warning("To visualize the posterior,
+            specify data x and n")
+
+  g <- ggplot(NULL, aes(x = c(0, 1))) +
+    labs(x = expression(pi),
+         y = "density") +
+    scale_fill_manual("",
+                      values = c(prior = "gold1",
+                                 `(scaled) likelihood` = "cyan2",
+                                 posterior = "cyan4"),
+                      breaks = c("prior",
+                                 "(scaled) likelihood",
+                                 "posterior"))
   if (prior == TRUE) {
-    g <- g + stat_function(fun = dbeta, args = list(shape1 = alpha,
-                                                    shape2 = beta)) + stat_function(fun = dbeta, args = list(shape1 = alpha,
-                                                                                                             shape2 = beta), geom = "area", alpha = 0.5, aes(fill = "prior"))
-  }
+    g <- g +
+      stat_function(fun = dbeta,
+                           args = list(shape1 = alpha,
+                                       shape2 = beta)) +
+      stat_function(fun = dbeta,
+                    args = list(shape1 = alpha,
+                                shape2 = beta),
+                    geom = "area",
+                    alpha = 0.5,
+                    aes(fill = "prior"))
+    }
+
   if (!is.null(x) & !is.null(n)) {
     alpha_post <- alpha + x
     beta_post <- beta + n - x
@@ -24,14 +59,23 @@ plot_beta_binomial <- function (alpha, beta, x = NULL, n = NULL, prior = TRUE, l
     }
   }
   if (!is.null(x) & !is.null(n) & (likelihood != FALSE)) {
-    g <- g + stat_function(fun = like_scaled) + stat_function(fun = like_scaled,
-                                                              geom = "area", alpha = 0.5, aes(fill = "(scaled) likelihood"))
+    g <- g +
+      stat_function(fun = like_scaled) +
+      stat_function(fun = like_scaled,
+                    geom = "area",
+                    alpha = 0.5,
+                    aes(fill = "(scaled) likelihood"))
   }
   if (!is.null(x) & !is.null(n) & posterior == TRUE) {
-    g <- g + stat_function(fun = dbeta, args = list(shape1 = alpha_post,
-                                                    shape2 = beta_post)) + stat_function(fun = dbeta,
-                                                                                         args = list(shape1 = alpha_post, shape2 = beta_post),
-                                                                                         geom = "area", alpha = 0.5, aes(fill = "posterior"))
+    g <- g +
+      stat_function(fun = dbeta,
+                    args = list(shape1 = alpha_post,
+                                shape2 = beta_post)) +
+      stat_function(fun = dbeta,
+                    args = list(shape1 = alpha_post,
+                                shape2 = beta_post),
+                    geom = "area", alpha = 0.5,
+                    aes(fill = "posterior"))
   }
   g
-}
+} # end of function
