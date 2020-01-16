3e7de0eb157cfe16a7a7694648d3b18f9f41722f
mdogucu <mdogucu@gmail.com>
2020-01-16 11:31:16 -0800

    Closes #9

--- R/plot_beta.R
+++ R/plot_beta.R
@@ -1,4 +1,16 @@
 
+#' @title Plot Beta Prior/Posterior
+#'
+#' @description Plots the Probability Density Function for Beta Prior/Posterior
+#'
+#' @param alpha,beta non-negative parameters of the Beta distribution 
+#'
+#' @return A ggplot with x axis set between 0 and 1
+#' @export
+#'
+#' @examples
+#' plot_beta(1,12)
+
 plot_beta <- function(alpha, beta){
   ggplot(data = data.frame(x = c(0, 1)),
          aes(x)) +
@@ -6,8 +18,7 @@ plot_beta <- function(alpha, beta){
                   n = 101,
                   args = list(shape1 = alpha,
                               shape2=beta)) +
-    labs(x = expression(pi), y =
-           expression(paste("f(",pi,")")))
+    labs(x = expression(pi), 
+         y = expression(paste("f(",pi,")")))
 }
 
-plot_beta(3, 17)
