ba177131a0910d055cb25b7a52585d22650da8ab
mdogucu <mdogucu@gmail.com>
2020-01-26 17:36:48 -0800

    Closes #6

--- R/summarize_poisson_gamma.R
+++ R/summarize_poisson_gamma.R
@@ -1,40 +1,66 @@
-summarize_poisson_gamma <- function (s, r, sum_x = NULL, n = NULL){
+#' Summarize the Poisson-Gamma Model
+#'
+#' @param shape A non-negative shape parameter of the Gamma distribution.
+#' @param rate A non-negative rate parameter of the Gamma distribution.
+#' @param sum_x sum of observed values for the Poisson likelihood.
+#' @param n number of observations for the Poisson likelihood.
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' #' \dontrun{
+#' summaruze_poisson_gamma(3, 4, 7, 12)
+#' }
+summarize_poisson_gamma <- function (shape,
+                                     rate,
+                                     sum_x = NULL,
+                                     n = NULL){
+
   if (is.null(sum_x) | is.null(n))
-    warning("To summarize the posterior, specify data sum_x and n")
+    warning("To summarize the posterior,
+            specify data sum_x and n")
   if (is.null(sum_x) & is.null(n)) {
-    prior_mean <- s/r
-    if (s >= 1) {
-      prior_mode <- (s - 1)/r
+    prior_mean <- shape/rate
+    if (shape >= 1) {
+      prior_mode <- (shape - 1)/rate
     }
     else {
       prior_mode <- NA
     }
-    prior_var <- s/r^2
-    return(data.frame(model = c("prior"), s = s, r = r, mean = prior_mean,
-                      mode = prior_mode, var = prior_var))
+    prior_var <- shape/rate^2
+    return(data.frame(model = c("prior"),
+                      shape = shape,
+                      rate = rate,
+                      mean = prior_mean,
+                      mode = prior_mode,
+                      var = prior_var))
   }
   else {
-    prior_mean <- s/r
-    if (s >= 1) {
-      prior_mode <- (s - 1)/r
+    prior_mean <- shape/rate
+    if (shape >= 1) {
+      prior_mode <- (shape - 1)/rate
     }
     else {
       prior_mode <- NULL
     }
-    prior_var <- s/r^2
-    post_mean <- (s + sum_x)/(r + n)
-    if ((s + sum_x) >= 1) {
-      post_mode <- (s + sum_x - 1)/(r + n)
+    prior_var <- shape/rate^2
+    post_mean <- (shape + sum_x)/(rate + n)
+    if ((shape + sum_x) >= 1) {
+      post_mode <- (shape + sum_x - 1)/(rate + n)
     }
     else {
       post_mode <- NA
     }
-    post_var <- (s + sum_x)/((r + n)^2)
-    post_s <- s + sum_x
-    post_r <- r + n
-    return(data.frame(model = c("prior", "posterior"), s = c(s,
-                                                             post_s), r = c(r, post_r), mean = c(prior_mean, post_mean),
-                      mode = c(prior_mode, post_mode), var = c(prior_var,
-                                                               post_var)))
+    post_var <- (shape + sum_x)/((rate + n)^2)
+    post_s <- shape + sum_x
+    post_r <- rate + n
+    return(data.frame(model = c("prior",
+                                "posterior"),
+                      shape = c(shape, post_s),
+                      rare = c(rate, post_r),
+                      mean = c(prior_mean, post_mean),
+                      mode = c(prior_mode, post_mode),
+                      var = c(prior_var, post_var)))
   }
 }
