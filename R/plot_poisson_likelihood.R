fecf773c4be0e59e815268f81d874cc2954c7150
mdogucu <mdogucu@gmail.com>
2020-01-29 10:33:32 -0800

    Closes #20

---
+++ R/plot_poisson_likelihood.R
@@ -0,0 +1,30 @@
+#' @title Plot Poisson Likelihood
+#'
+#' @param x vector of observed data
+#' @param lambda_upper_bound upper bound for lambda
+#'
+#' @return a ggplot of Poisson likelihood
+#' @export
+#' 
+#' @import ggplot2
+#'
+#' @examples
+#' 
+plot_poisson_likelihood <- 
+  function(x = c(4,2,7), 
+           lambda_upper_bound = 10){
+    
+    lambda = seq(0, lambda_upper_bound, by = 0.1)
+    
+    y = exp(-1*length(x)*lambda)*lambda^(sum(x))/prod(factorial(x))
+    
+    data <- data.frame(lambda = lambda,
+                       f_lambda = 
+                         rep(y, length(lambda)))
+    ggplot(data, aes(x = lambda, 
+                     y = f_lambda)) +
+      geom_line() +
+      labs(x = expression(lambda),
+           y = expression(paste("L(",lambda,"|(X=", x, "))")))
+    
+}
