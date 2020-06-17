39bb6ed6f4764322e564025939c7bfc07dfc11df
mdogucu <mdogucu@gmail.com>
2020-06-17 11:50:07 -0700

    closes #40

--- R/plot_binomial_likelihood.R
+++ R/plot_binomial_likelihood.R
@@ -2,29 +2,33 @@
 #'
 #' @param x number of successes  
 #' @param n number of trials
-#' @param pi sample space of pi
 #' @param mle a logical value indicating whether maximum likelihood estimation should be plotted
-#'
+#' @param pi support of pi
 #' @return
 #' @export
 #' 
 #' @import magrittr
+#' @importFrom dplyr filter
 #'
 #' @examples
 plot_binomial_likelihood <-function(x, 
                                     n, 
-                                    pi = seq(0, 1, by = 0.01), 
-                                    mle = FALSE){
+                                    mle = FALSE,
+                                    pi = seq(0, 1, by = 0.05)){
   
-  likelihood <- dbinom(x = x, size = n, prob = pi)
+  message("Note that this function shows discrete values of pi.
+          You can trick it into making it look more like continuous by defining a better argument for pi")
+
+
+  lhood <- dbinom(x = x, size = n, prob = pi)
   
-  data <- data.frame(likelihood = likelihood, pi = pi)
+  data <-data.frame(likelihood = lhood, pi = pi)
   
   max_data <- data %>% 
-    filter(likelihood == max(likelihood))
+    dplyr::filter(likelihood == max(likelihood))
   
   g <- data %>% 
-    ggplot(aes(x = pi, y = likelihood)) +
+    ggplot(aes(x = pi, y = lhood)) +
     
     geom_point() +
     
@@ -32,7 +36,7 @@ plot_binomial_likelihood <-function(x,
                  x = pi, 
                  xend = pi, 
                  y = 0, 
-                 yend = likelihood) +
+                 yend = lhood) +
     
     labs(x = expression(pi),
          y = expression(paste("L(",pi,"|(X=", x, "))")))
