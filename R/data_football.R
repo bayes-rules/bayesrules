66d83a6bc9cbfc8d497c268cd2672b3a99077dce
mdogucu <mdogucu@gmail.com>
2020-10-06 20:52:37 -0700

    add football data closes #52

---
+++ R/data_football.R
@@ -0,0 +1,16 @@
+#' Football Brain Measurements
+#' 
+#' Brain measurements for football and non-football players as provided in the Lock5 package
+#' 
+#' @format A data frame with 75 observations and 5 variables:
+#' \describe{
+#'   \item{group}{control = no football, 
+#'   fb_no_concuss = football player but no concussions, 
+#'   gb_concuss = football player with concussion history}
+#'   \item{hipp}{Total hippocampus volume, in microL}
+#'   
+#' @source Singh R, Meier T, Kuplicki R, Savitz J, et al., 
+#' "Relationship of Collegiate Football Experience and Concussion 
+#' With Hippocampal Volume and Cognitive Outcome," JAMA, 311(18), 2014
+#' 
+"football"
---
+++ data-raw/football.R
@@ -0,0 +1,12 @@
+library(tidyverse)
+
+football <- Lock5Data::FootballBrain %>% 
+rename(group = Group,
+         hipp = Hipp) %>% 
+  mutate(group = case_when(
+    group == "no football" ~ "no_football",
+    group == "FBNoConcuss" ~ "fb_no_concuss",
+    group == "FBConcuss" ~ "fb_concuss"
+  )) %>% 
+  select(group, hipp)
+usethis::use_data(football, overwrite = TRUE)
