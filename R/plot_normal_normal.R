c05954c79b9964c15d111fae59eb0f63e18744ff
Alicia Johnson <ajohns24@users.noreply.github.com>
2020-11-03 14:24:13 -0600

    change x to y

--- R/plot_normal_normal.R
+++ R/plot_normal_normal.R
@@ -3,7 +3,7 @@
 #' @param mean prior mean
 #' @param sd prior standard deviation
 #' @param sigma likelihood standard deviation
-#' @param x_bar sample mean of the data
+#' @param y_bar sample mean of the data
 #' @param n sample size of the data
 #' @param prior a logical value indicating whether the prior model should be plotted.
 #' @param likelihood a logical value indicating whether the scaled likelihood should be plotted.
@@ -14,29 +14,29 @@
 #' @import ggplot2
 #' @examples
 #' 
-#' plot_normal_normal(mean = 0, sd = 3, sigma= 4, x_bar = 5, n = 3)
-#' plot_beta_binomial(mean = -1, sd = 5, sigma= 4, x_bar = 5, n = 3, posterior = FALSE)
+#' plot_normal_normal(mean = 0, sd = 3, sigma= 4, y_bar = 5, n = 3)
+#' plot_beta_binomial(mean = -1, sd = 5, sigma= 4, y_bar = 5, n = 3, posterior = FALSE)
 
 plot_normal_normal <- function (mean, 
                                      sd, 
                                      sigma = NULL,
-                                     x_bar = NULL, 
+                                     y_bar = NULL, 
                                      n = NULL,
                                      prior = TRUE,
                                      likelihood = TRUE,
                                      posterior = TRUE)
 {
-  if (is.null(x_bar) | is.null(n)|is.null(sigma))
+  if (is.null(y_bar) | is.null(n)|is.null(sigma))
     warning("To plot the posterior, 
-            specify sigma for the likelihood, data xbar and n")
+            specify sigma for the likelihood, data ybar and n")
   
   
-      post_mean <- (((sigma^2)*mean) + ((sd^2)*n*x_bar))/(n*(sd^2)+(sigma^2))
+      post_mean <- (((sigma^2)*mean) + ((sd^2)*n*y_bar))/(n*(sd^2)+(sigma^2))
       post_var <- ((sigma^2)*(sd^2))/(n*(sd^2)+(sigma^2))
       
       
-      x <- c(min(c(mean - 4*sd, x_bar-4*sigma/sqrt(n))) , 
-             max(c(mean + 4*sd, x_bar+4*sigma/sqrt(n))))
+      x <- c(min(c(mean - 4*sd, y_bar-4*sigma/sqrt(n))) , 
+             max(c(mean + 4*sd, y_bar+4*sigma/sqrt(n))))
       
       g<-ggplot(data = data.frame(x = x), aes(x)) +
         labs(x = expression(mu),
@@ -61,19 +61,19 @@ plot_normal_normal <- function (mean,
                         alpha = 0.5,
                         aes(fill = "prior"))
       }
-      if (!is.null(x_bar) & !is.null(n) & !is.null(sigma) & (likelihood != FALSE)) {
+      if (!is.null(y_bar) & !is.null(n) & !is.null(sigma) & (likelihood != FALSE)) {
         g <- g +
           stat_function(fun = dnorm,
-                        args = list(mean = x_bar,
+                        args = list(mean = y_bar,
                                     sd = sigma/sqrt(n))) +
           stat_function(fun = dnorm,
-                        args = list(mean = x_bar,
+                        args = list(mean = y_bar,
                                     sd = sigma/sqrt(n)),
                         geom = "area",
                         alpha = 0.5,
                         aes(fill = "(scaled) likelihood"))
       }
-      if (!is.null(x_bar) & !is.null(n) & !is.null(sigma) & posterior == TRUE) {
+      if (!is.null(y_bar) & !is.null(n) & !is.null(sigma) & posterior == TRUE) {
         g <- g +
           stat_function(fun = dnorm,
                         args = list(mean = post_mean,
