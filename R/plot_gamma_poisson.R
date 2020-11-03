e8279ae17b01373a5f71ff22dee4abd1b619932e
Alicia Johnson <ajohns24@users.noreply.github.com>
2020-11-03 14:20:23 -0600

    change x to y

--- R/plot_gamma_poisson.R
+++ R/plot_gamma_poisson.R
@@ -2,7 +2,7 @@
 #'
 #' @param shape A non-negative shape parameter of the Gamma distribution.
 #' @param rate A non-negative rate parameter of the Gamma distribution.
-#' @param sum_x sum of observed values for the Poisson likelihood.
+#' @param sum_y sum of observed values for the Poisson likelihood.
 #' @param n number of observations for the Poisson likelihood.
 #' @param prior a logical value indicating whether the prior distribution should be plotted.
 #' @param likelihood a logical value indicating whether the scaled likelihood should be plotted.
@@ -14,30 +14,30 @@
 #' @import ggplot2
 #'
 #' @examples
-#' plot_gamma_poisson(shape = 100, rate = 20, sum_x = 39, n = 6)
+#' plot_gamma_poisson(shape = 100, rate = 20, sum_y = 39, n = 6)
 
 plot_gamma_poisson <- function (shape, rate,
-                                sum_x = NULL,
+                                sum_y = NULL,
                                 n = NULL,
                                 prior = TRUE,
                                 likelihood = TRUE,
                                 posterior = TRUE){
   
-  if (is.null(sum_x) | is.null(n))
+  if (is.null(sum_y) | is.null(n))
     warning("To visualize the posterior,
-            specify information about the data: sum_x and n")
+            specify information about the data: sum_y and n")
   
  
   
   x_min <- min(qgamma(1e-05, shape, rate),
                qgamma(1e-05,
-                      shape + sum_x,
+                      shape + sum_y,
                       rate + n),
-               qgamma(1e-05, sum_x+1, n))
+               qgamma(1e-05, sum_y + 1, n))
   x_max <- max(qgamma(0.99999, shape, rate),
-               qgamma(0.99999, shape + sum_x,
+               qgamma(0.99999, shape + sum_y,
                       rate + n),
-               qgamma(0.99999, sum_x+1, n))
+               qgamma(0.99999, sum_y + 1, n))
   g <- ggplot(data = data.frame(x = c(x_min, x_max)), aes(x)) +
     labs(x = expression(lambda),
          y = "density") +
@@ -61,14 +61,14 @@ plot_gamma_poisson <- function (shape, rate,
                     aes(fill = "prior"))
   }
 
-  if (!is.null(sum_x) & !is.null(n)) {
-    shape_post <- shape + sum_x
+  if (!is.null(sum_y) & !is.null(n)) {
+    shape_post <- shape + sum_y
     rate_post <- rate + n
     like_scaled <- function(x) {
-      dgamma(x, shape = sum_x + 1, rate = n)
+      dgamma(x, shape = sum_y + 1, rate = n)
     }
   }
-  if (!is.null(sum_x) & !is.null(n) & (likelihood != FALSE)) {
+  if (!is.null(sum_y) & !is.null(n) & (likelihood != FALSE)) {
     g <- g +
       stat_function(fun = like_scaled) +
       stat_function(fun = like_scaled,
@@ -76,7 +76,7 @@ plot_gamma_poisson <- function (shape, rate,
                     alpha = 0.5,
                     aes(fill = "(scaled) likelihood"))
   }
-  if (!is.null(sum_x) & !is.null(n) & posterior == TRUE) {
+  if (!is.null(sum_y) & !is.null(n) & posterior == TRUE) {
     g <- g +
       stat_function(fun = dgamma,
                     args = list(shape = shape_post,
