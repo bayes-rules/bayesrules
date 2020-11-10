e35b31d9e5311c9ea3fa5807439e7f2ac87598fb
Alicia Johnson <ajohns24@macalester.edu>
2020-11-09 18:01:02 -0600

    close #56

--- data-raw/football.R
+++ data-raw/football.R
@@ -6,7 +6,8 @@ rename(group = Group,
   mutate(group = case_when(
     group == "no football" ~ "no_football",
     group == "FBNoConcuss" ~ "fb_no_concuss",
-    group == "FBConcuss" ~ "fb_concuss"
+    group == "FBConcuss" ~ "fb_concuss",
+    group == "Control" ~ "control"
   )) %>% 
   select(group, hipp)
 usethis::use_data(football, overwrite = TRUE)
