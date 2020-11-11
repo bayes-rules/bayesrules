2dd68ef93b58dddd85f0e8de7e2c378b23192430
Alicia Johnson <ajohns24@macalester.edu>
2020-11-11 16:26:00 -0600

    add cherry blossom data

---
+++ data-raw/cherry_blossom_sample.R
@@ -0,0 +1,19 @@
+library(dplyr)
+library(mdsr)
+data(Cherry)
+
+# I.D. subjects that have 7 observations
+subj <- Cherry %>%
+          filter(nruns == 7) %>% 
+          group_by(name.yob) %>%
+          summarize(min_age = min(age)) %>%
+          filter(min_age >= 50, min_age < 55) %>% 
+          mutate(subject = as.factor(c(1:length(name.yob))))
+
+
+cherry_blossom_sample <- Cherry %>%
+          filter(name.yob %in% subj$name.yob) %>% 
+          left_join(subj) %>% 
+          select(subject, age, net, gun, year, previous)
+
+usethis::use_data(cherry_blossom_sample, overwrite = TRUE)
