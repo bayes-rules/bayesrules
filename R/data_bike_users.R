36cbb170dd0095dfac89505f438349fecbf093d9
Alicia Johnson <ajohns24@macalester.edu>
2020-11-11 13:53:26 -0600

    add bike_users data

---
+++ R/data_bike_users.R
@@ -0,0 +1,23 @@
+#' Capital Bikeshare Bike Ridership (Registered & Casual Riders)
+#' 
+#' Data on ridership among registered members and casual users of the Capital Bikeshare service in Washington, D.C..
+#' 
+#' @format A data frame with 534 daily observations, 267 each for registered riders and casual riders, and 13 variables:
+#' \describe{
+#'   \item{date}{date of observation}
+#'   \item{season}{fall, spring, summer, or winter}
+#'   \item{year}{the year of the date}
+#'   \item{month}{the month of the date}
+#'   \item{day_of_week}{the day of the week}
+#'   \item{weekend}{whether or not the date falls on a weekend (TRUE or FALSE)}
+#'   \item{holiday}{whether or not the date falls on a holiday (yes or no)}
+#'   \item{temp_actual}{raw temperature (degrees Fahrenheit)}
+#'   \item{temp_feel}{what the temperature feels like (degrees Fahrenheit)}
+#'   \item{humidity}{humidity level (percentage)}
+#'   \item{windspeed}{wind speed (miles per hour)}
+#'   \item{weather_cat}{weather category (categ1 = pleasant, categ2 = moderate, categ3 = severe)}
+#'   \item{user}{rider type (casual or registered)}
+#'   \item{rides}{number of bikeshare rides}
+#'   }
+#' @source Fanaee-T, Hadi and Gama, Joao (2013). Event labeling combining ensemble detectors and background knowledge. Progress in Artificial Intelligence. \url{https://archive.ics.uci.edu/ml/datasets/Bike+Sharing+Dataset}
+"bike_users"
---
+++ data-raw/bike_users.R
@@ -0,0 +1,12 @@
+# Original source: https://archive.ics.uci.edu/ml/datasets/Bike+Sharing+Dataset
+bikes <- read.csv("https://www.macalester.edu/~ajohns24/data/bike_share.csv")
+
+# Clean up & subset
+set.seed(84735)
+bike_users <- bikes %>% 
+          gather(user, rides, -c(date,season,year,month,day_of_week,weekend,holiday,temp_actual,temp_feel,humidity,windspeed,weather_cat)) %>% 
+          mutate(user = factor(user, labels = c("casual","registered","total"))) %>% 
+          filter(user != "total", year == 2011) %>% 
+          mutate(user = droplevels(user)) %>% 
+          filter(temp_feel < 87, temp_feel > 45) 
+usethis::use_data(bike_users, overwrite = TRUE)
