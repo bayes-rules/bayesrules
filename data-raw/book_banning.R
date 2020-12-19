53601af684b76f06e53393645f41c5ff59b29269
Alicia Johnson <ajohns24@macalester.edu>
2020-12-18 18:14:32 -0600

    add roback and legler book banning data

---
+++ data-raw/book_banning.R
@@ -0,0 +1,15 @@
+library(RCurl)
+
+x <- getURL("https://raw.githubusercontent.com/proback/BeyondMLR/master/data/bookbanningNoTex.csv")
+book_banning <- read.csv(text = x) %>% 
+          rename(title = booktitle, college_grad_rate = cperba, date = days2000,
+                 lgbtq = homosexuality, median_income = cmedin, book_id = book,
+                 explicit = sexexp, hs_grad_rate = cperhs, political_value_index = pvi2) %>% 
+          select(-X, -obama, -freqchal) %>% 
+          mutate(date = as.Date(date, origin = "2000-01-01")) %>% 
+          mutate(year = lubridate::year(date)) %>% 
+          select(title, book_id, author, date, year, removed, 
+                 explicit, antifamily, occult, language, lgbtq, violence,
+                 state, political_value_index, median_income, hs_grad_rate, college_grad_rate)
+
+usethis::use_data(book_banning, overwrite = TRUE)
