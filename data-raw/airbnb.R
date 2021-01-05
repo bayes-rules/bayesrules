8215f519eab595e0f760f92bd2980fdf386e3e0a
Alicia Johnson <ajohns24@macalester.edu>
2021-01-04 18:48:00 -0600

    add airbnb data

---
+++ data-raw/airbnb.R
@@ -0,0 +1,9 @@
+library(RCurl)
+
+x <- getURL("https://raw.githubusercontent.com/proback/BeyondMLR/master/data/airbnb.csv")
+airbnb <- read.csv(text = x) %>% 
+          select(-X, -PctBlack) %>% 
+          rename(minimum_stay = minstay, walk_score = WalkScore, 
+                 transit_score = TransitScore, bike_score = BikeScore)
+          
+usethis::use_data(airbnb, overwrite = TRUE)
