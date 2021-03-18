0b41dc34afa5ff0f9d64a6ff221598c0787b45ab
Alicia Johnson <ajohns24@macalester.edu>
2021-03-18 12:43:24 -0500

    make subjects factors

--- data-raw/cherry_blossom_sample.R
+++ data-raw/cherry_blossom_sample.R
@@ -14,6 +14,7 @@ subj <- Cherry %>%
 cherry_blossom_sample <- Cherry %>%
           filter(name.yob %in% subj$name.yob) %>% 
           left_join(subj) %>% 
-          select(subject, age, net, gun, year, previous)
+          select(subject, age, net, gun, year, previous) %>% 
+          mutate(subject = as.factor(subject))
 
 usethis::use_data(cherry_blossom_sample, overwrite = TRUE)
