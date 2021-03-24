8d831515eaa60ee982e4f380c2a5cb9786e3132a
Alicia Johnson <ajohns24@macalester.edu>
2021-03-24 09:13:53 -0500

    make things factors

--- data-raw/airbnb.R
+++ data-raw/airbnb.R
@@ -5,6 +5,7 @@ airbnb <- read.csv(text = x) %>%
           select(-X, -PctBlack) %>% 
           rename(minimum_stay = minstay, walk_score = WalkScore, 
                  transit_score = TransitScore, bike_score = BikeScore,
-                 rating = overall_satisfaction)
+                 rating = overall_satisfaction) %>% 
+          mutate(neighborhood = as.factor(neighborhood), district = as.factor(district), room_type = as.factor(room_type))
           
 usethis::use_data(airbnb, overwrite = TRUE)
