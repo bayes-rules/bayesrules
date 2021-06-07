7326151ac49762752859e21865645c8030417052
Alicia Johnson <ajohns24@macalester.edu>
2021-06-07 08:37:44 -0500

    fix dependencies

--- data-raw/big_word_club/big_word_clubprep.R
+++ data-raw/big_word_club/big_word_clubprep.R
@@ -1,5 +1,9 @@
-big_word_club <- read_csv("data-raw/big_word_club/big_word_club.csv") %>% 
-  mutate(score_pct_change = (score_a2 - score_a1) / score_a1 * 100)
+library(dplyr)
+library(here)
+
+big_word_club <- read.csv(here("data-raw", "big_word_club", "big_word_club.csv")) %>% 
+  mutate(score_pct_change = (score_a2 - score_a1) / score_a1 * 100) %>% 
+  mutate(school_id = as.factor(school_id))
 
 # The .pdf file type is not great in packages. 
 # The codebook can be accessed from an earlier commit https://github.com/mdogucu/bayesrules/blob/d9a01160ddf5bebb4a1ac2bb9869d539a1c63589/data-raw/big_word_club/BWC%20Codebook.pdf
